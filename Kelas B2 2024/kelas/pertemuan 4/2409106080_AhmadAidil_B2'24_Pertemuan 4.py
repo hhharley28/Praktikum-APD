fba2428b09bbba7b39b603614d50d2abee1999a9
hhharley28 <ahmadaidil873@gmail.com>
2024-09-27 15:11:15 +0800

    upload file coding kelas

---
+++ Kelas B2 2024/kelas/pertemuan 4/2409106080_AhmadAidil_B2'24_Pertemuan 4.py
@@ -0,0 +1,74 @@
+#  ulang = 10
+#  for i in range(ulang):
+#      print("perulangan ke-"+str(i))
+     #print(f"perulangan ke-{i} jamun")
+
+# for i in range (1, 11, 2):
+#     print (f"perulangan ke-- {i} kali")
+
+# simpan = [12, "Aidil gnteng, 14.5, True,'A' "]
+# for i in (simpan) :
+#     print (i)
+
+# print(simpan[2])
+
+# for i in range (1, 4):
+#     for j in range (1, 4):
+#         print(f"{i} X {j} = {i*j}")
+#     print()
+
+# for i in range (1, 6, 2):
+#     for j in range (1, 6, 2):
+#         print(f"{i} X {j} = {i*j}")
+#     print()
+
+
+#  for ayam in range( 0, -1, -1):
+#      print(f"anak ayam turun {ayam}" )
+
+# ulang = "ya"
+# hitung = 1
+# while ulang == "ya":
+#     print(f"perulangan ke {hitung + 1}")
+#     ulang = input ("apakah anda masih ingin mengeulang?")
+#     hitung +=1
+# print("perulangan selesai")
+
+# x = 0
+# while x < 5
+#     print(x)
+#     x+=1
+
+# hitung=1
+# while True :
+#     ulang = input("apakah anda masih ingin mengulangi?")
+#     if ulang == "tidak":
+#         print ("perulangan berhenti")
+#         break
+#     hitung +=1
+
+# print (f"total perulangan: {hitung}")
+
+# for i in range (5):
+#      if i % 2 == 0 :
+#          continue 
+#      if i  == 5:
+#          break
+#      print(i)
+
+# id = "admin"
+# pw = "123"
+# salah = 0
+
+# while salah < 3:
+#     username = input ("masukkan id=")
+#     password = input ("masukkan pw=")
+#     if id==username and pw==password :
+#         print("login berhasil")
+#         break
+#     else:   
+#         print ("login gagal")
+#         salah += 1
+
+
+
