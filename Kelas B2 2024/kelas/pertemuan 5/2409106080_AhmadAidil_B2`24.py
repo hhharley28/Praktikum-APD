87ee8c120bffe226c5a04416cf95b97837969b01
hhharley28 <ahmadaidil873@gmail.com>
2024-10-04 15:40:19 +0800

    Add files via upload

---
+++ Kelas B2 2024/kelas/pertemuan 5/2409106080_AhmadAidil_B2`24.py
@@ -0,0 +1,76 @@
+# list = ["a",2,True]
+
+# print (list (-1(2)))
+
+# list = ["a",2,]
+
+# tas = ["buku", 32, True, 3.14, ["IF",24]]
+
+# for i in range (len(tas)):
+#     print (tas[i])
+
+# matkul = ["kalkulus" , "fisdas" , "pti" ]
+# print (matkul)
+
+# matkul.append("sastra mesin")
+# matkul [3] = "sastra mesin"
+# print (matkul)
+
+# matkul.insert(1,"sastra mesin")
+# print(matkul)
+# matkul [1] ="pancasila"
+# print matkul
+
+# del matkul [1]
+# print matkul
+
+# buang = matkul.pop(1)
+# print (matkul)
+
+# print(f"variabel buang = {buang}")
+
+# prodi = ["it", "sisfor" , "elektro", "arsi","lingkungan", "kimia"]
+# print (prodi[::2]"\:n",prodi )
+
+# barang = [ ["sepeatu", "kaus kaki", "Sarung"],
+#            ["Pulpen", "Pensil", "Laptop"] ]
+# print(barang[0][-2])
+# #memanggil list secara keseluruhan untuk melihat bagaimana outputnya
+# print(barang, "\n")
+# #memanggil indeks pertama
+# print(barang[0], "\n")
+#kali ini kita ingin menampilkan elemen “Pulpen”, kita perlu mengakses indeks dua kali, yang pertama untuk memilih list dengan elemen yang kita inginkan dan yang kedua indeks
+# elemen itu sendiri
+# print(barang[1][0])
+
+# print (barang)
+# # for i in barang:
+# #     for j in i :
+# #         print (j)
+# for baris in barang:
+#     for kolom in baris :
+#         print (kolom)
+
+# nama = ("aldi", "boy", "julian")
+# print (nama)
+# nama.append("rian")
+#  print (nama)
+
+# barang1 = [""]
+
+# mahasiswa = (69, "Informatika", "2209106044", "Aldy septian ")
+# #lalu kita unpacking
+# absen, prodi, nim, nama= mahasiswa
+# #maka ketiga variabel tersebut akan berisi data dari tuple mahasiswa
+# #menampilkan variabel
+# print(absen)
+# print(prodi)
+# print(nim)
+# print(nama)
+
+# print (mahasiswa)
+# mahasiswa = list (mahasiswa)
+# mahasiswa[2] = "2409106080"
+
+# print = mahasiswa
+
