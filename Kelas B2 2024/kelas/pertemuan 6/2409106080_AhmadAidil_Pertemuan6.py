2f2b5fd9f7f6f80902119824ed5c6786e72c6767
hhharley28 <ahmadaidil873@gmail.com>
2024-10-11 15:20:06 +0800

    Add files via upload

---
+++ Kelas B2 2024/kelas/pertemuan 6/2409106080_AhmadAidil_Pertemuan6.py
@@ -0,0 +1,91 @@
+# daftar_buku = {
+# "Buku1" : "Harry Potter",
+# "Buku2" : "Percy Jackson",
+# "Buku3" : "Twillight"
+# }
+# print(daftar_buku["Buku1"])
+# print(daftar_buku["Buku2"])
+# print(daftar_buku["Buku3"])
+
+
+# daftar_buku = {}
+
+# daftar_buku ["novel 1"] = "senyum pertama di pagi hari adalah ibadah"
+
+# print (daftar_buku )
+
+
+
+# daftar_buku = dict(Buku1 = "Harry potter", Buku2 = "percy jackson")
+# print (daftar_buku)
+
+# print (daftar_buku.get("Buku2"
+
+# Nilai = {
+#     "Matematika" : 80,
+#     "B. Indonesia" : 90,
+#     "B. Inggris" : 81,
+#     "Kimia" : 78,
+#     "Fisika" : 80
+# }
+
+# print (Nilai)
+# for i in Nilai:
+#     print (i)
+
+# for i, j in Nilai.items():
+#     print(f"nilai dari {i} itu valuenya {j}")
+
+# Nilai.update({"sturuktur data" : 99})
+# Nilai.update({"matematika" : 300})
+
+# trashbin = Nilai.pop("Matematika")
+# print(Nilai)
+# print()
+# print (trashbin)
+
+# del Nilai["Fisika"]
+# print()
+# print(Nilai)
+
+# Nilai.clear
+# print(Nilai)
+
+# print(f"jumlah elemen yang variabel diaats adalah {len(Nilai)}")
+
+# Buku = {
+# "No Longer Human" : "Osamu Dazai",
+# "Harry Potter" : "J.K. Rowlings",
+# "Hamlet" : "William Shakespeare"
+# }
+# pinjam = Buku.copy()
+# print("Dictionary yang Telah Disalin : ", pinjam)
+
+# daftar_nilai = Nilai.copy()
+# print(daftar_nilai)
+
+# kunci = "apel", "jeruk", "mangga"
+# value = 7
+# kunci2 = dict.fromkeys(kunci, value)
+# print(kunci2)
+
+# Musik = {
+#     "The Chainsmoker" : ["All we Know", "The Paris"],
+#     "Alan Walker" : ["Alone", "Lily"],
+#     "Neffex" : ["Best of Me", "Memories"]
+#     }
+# for i, j in Musik.items():
+#         print(f"Musik milik {i} adalah : ")
+#         for song in j:
+#                 print(song)
+#                 print("")
+
+# mahasiswa = {
+# 101 : {"Nama" : "Aldy", "Umur" : 19},
+# 111 : {"Nama" : "Abdul", "Umur" : 18 "Hobi" : ["membaca", "menulis","bola"]}
+# }
+# for key, value in mahasiswa.items():
+#     print("ID Mahasiswa : ", key)
+#     for key_a, value_a in value.items():
+#         print (key_a, " : ", value_a)
+#         print("")
