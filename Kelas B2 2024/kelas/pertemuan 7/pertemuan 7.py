6f01432233123dd901682d570c4b93f7ebccba67
hhharley28 <ahmadaidil873@gmail.com>
2024-10-18 15:10:20 +0800

    Add files via upload

---
+++ Kelas B2 2024/kelas/pertemuan 7/pertemuan 7.py
@@ -0,0 +1,93 @@
+# def tambah () :
+#     print ("hello world")
+
+# tambah ()
+
+# def tambah (a,b):
+#     hasil = a+b
+
+#     print (hasil)
+
+
+# tambah (10,2)
+
+# def tambah (a,b):
+#     hasil = a+b
+#     return hasil
+# tambahan = tambah(2,4) + tambah (4,6)
+
+    
+# print (tambahan)
+
+# def pilihan (opsi):
+#     match opsi:
+#         case 1:
+#             print(1)
+#             return
+#         case 2:
+#             print (2)
+#             return
+        
+#     print(3)
+
+# # pilihan (2)
+
+# def cetak (nama,nim,**matkul):
+#     print (nama,nim,matkul)
+
+# var = 3,4,5
+
+# var3, var4, var5 = var
+# print (var3)
+# print (var4)
+# print (var5)
+
+
+# membuat variabel global
+# Nama = "Informatika"
+# Mata_Kuliah = "Algoritma dan Pemrograman Dasar"
+# kelas = "asik banget"
+# # membuat variabel lokal
+# def info():
+#     Nama = "Teknik Elektro"
+#     Mata_Kuliah = "Pengantar Teknik ELektro"
+#     kelas = "it asik"
+# # mengakses variabel lokal
+#     print("Prodi:", Nama)
+#     print("Mata Kuliah:", Mata_Kuliah)
+#     print (kelas)
+# # mengakses variabel global
+# print("Prodi:", Nama)
+# print("Mata Kuliah:", Mata_Kuliah)
+
+# info()
+
+# import pertemuan7
+
+# pertemuan7.cetak("fathir",71)
+# if__name__="__main__"
+#     def cetak
+
+# fungsi untuk menampilkan menu
+# def show_menu():
+#     print ("\n")
+#     print ("----------- MENU---------- ")
+#     print ("[1] Show Data")
+#     print ("[2] Insert Data")
+#     print ("[3] Edit Data")
+#     print ("[4] Delete Data")
+#     print ("[5] Exit")
+# menu = input("PILIH MENU> ")
+# print ("\n")
+# if menu == "1":
+#     show_data()
+# elif menu == "2":
+#     insert_data()
+# elif menu == "3":
+#     edit_data()
+# elif menu == "4":
+#     delete_data()
+# elif menu == "5":
+#     exit()
+# else:
+#     print ("Salah pilih!")
